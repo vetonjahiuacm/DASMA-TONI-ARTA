3dc70046e8d8791279127be4365e9435bf976c00
vetonjahiuacm <58882075+vetonjahiuacm@users.noreply.github.com>
2026-08-09 21:40:56 +0200

    api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,31 +1,54 @@
-import axios from "axios";
+import emailjs from "@emailjs/browser";
 
-const BACKEND_URL =
-  process.env.REACT_APP_BACKEND_URL ||
-  "https://vetoniartadasme.site";
+// EmailJS configuration
+const SERVICE_ID = "service_5lthul6";
+const TEMPLATE_ID = "__ejs-test-mail-service__";
+const PUBLIC_KEY = "o0sDT0GCLXynbkP42";
 
-const API = BACKEND_URL + "/api";
+export const submitRsvp = async (data) => {
+  try {
+    const templateParams = {
+      name: data.name || "",
+      attending: data.attending || "",
+      guests: data.guests || 0,
+      message: data.message || "",
+    };
 
-export function submitRsvp(data) {
-  return axios
-    .post(API + "/rsvp", data)
-    .then(function (response) {
-      return response.data;
-    });
-}
+    const response = await emailjs.send(
+      SERVICE_ID,
+      TEMPLATE_ID,
+      templateParams,
+      {
+        publicKey: PUBLIC_KEY,
+      }
+    );
 
-export function fetchRsvps() {
-  return axios
-    .get(API + "/rsvps")
-    .then(function (response) {
-      return response.data;
-    });
-}
+    console.log("RSVP email sent:", response.status, response.text);
 
-export function fetchSeats() {
-  return axios
-    .get(API + "/seats")
-    .then(function (response) {
-      return response.data;
-    });
-}
+    return {
+      success: true,
+      message: "Konfirmimi u dërgua me sukses!",
+    };
+  } catch (error) {
+    console.error("RSVP email error:", error);
+
+    throw new Error(
+      "Nuk u dërgua konfirmimi. Ju lutemi provoni përsëri."
+    );
+  }
+};
+
+export const fetchRsvps = async () => {
+  return [];
+};
+
+export const fetchSeats = async () => {
+  return {
+    total: 80,
+    confirmedGuests: 0,
+    remaining: 80,
+    acceptedCount: 0,
+    declinedCount: 0,
+    totalResponses: 0,
+  };
+};
