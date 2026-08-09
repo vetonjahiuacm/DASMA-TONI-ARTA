789c23939dfb91e3ab813e09b9fcfddfbca282ee
vetonjahiuacm <58882075+vetonjahiuacm@users.noreply.github.com>
2026-08-09 19:22:03 +0200

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 import "./App.css";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
+
 import { Toaster } from "./components/ui/sonner";
 
 function App() {
   return (
     <div className="App paper-bg">
-      <BrowserRouter>
+      <BrowserRouter basename="/DASMA-TONI-ARTA">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/admin" element={<Admin />} />
         </Routes>
       </BrowserRouter>
+
       <Toaster position="top-center" />
     </div>
   );
