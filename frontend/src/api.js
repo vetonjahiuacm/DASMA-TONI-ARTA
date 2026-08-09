ce04098871446bc862c6d05543cca3e0c9c9470e
vetonjahiuacm <58882075+vetonjahiuacm@users.noreply.github.com>
2026-08-09 19:43:24 +0200

    api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,21 +1,19 @@
 import axios from "axios";
 
-const BACKEND_URL =
-  process.env.REACT_APP_BACKEND_URL || "https://api.vetoniartadasme.site";
-
-const API = ${BACKEND_URL}/api;
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "https://api.vetoniartadasme.site";
+const API = BACKEND_URL + "/api";
 
 export const submitRsvp = async (data) => {
-  const res = await axios.post(`${API}/rsvp`, data);
+  const res = await axios.post(API + "/rsvp", data);
   return res.data;
 };
 
 export const fetchRsvps = async () => {
-  const res = await axios.get(`${API}/rsvps`);
+  const res = await axios.get(API + "/rsvps");
   return res.data;
 };
 
 export const fetchSeats = async () => {
-  const res = await axios.get(`${API}/seats`);
+  const res = await axios.get(API + "/seats");
   return res.data;
 };
